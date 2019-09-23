ec69e5f7142eef00acc0e81d38becd1b657aabce
Szymon <sz.sniadach@gmail.com>
2019-09-24 00:41:15 +0200

    change email

--- src/email.php
+++ src/email.php
@@ -18,19 +18,19 @@ if (($_POST['name'] != '') && ($_POST['email'] != '') && ($_POST['message'] != '
             </body>
             </html>";
 
-    $mail = mail("sz.sniadach@wp.pl", "Wiadomość ze strony sniadach.pl", $msg, $header);
+    $mail = mail("sz.sniadach@gmail.com", "Wiadomość ze strony sniadach.pl", $msg, $header);
 
 
     $header  = 'MIME-Version: 1.0' . "\r\n";
     $header .= "Content-type: text/html; charset=UTF-8" . "\r\n";
-    $header .= 'FROM: Szymon Śniadach <sz.sniadach@wp.pl>' . "\r\n";
+    $header .= 'FROM: Szymon Śniadach <sz.sniadach@gmail.com>' . "\r\n";
         
     $date = date("d.m.Y");
         
     $msg = "<html>
             <body style=\"font-family: Arial\">
                 <h4>Twoja wiadomość została wysłana!</h4>
-                W dniu {$date} została wysłana wiadomość do sz.sniadach@wp.pl <br>
+                W dniu {$date} została wysłana wiadomość do sz.sniadach@gmail.com <br>
                 Poniżej treść Twojej wiadomości:<br>
                 <pre style=\"color:#228CE8\">{$_POST['message']}</pre>
             </body>
